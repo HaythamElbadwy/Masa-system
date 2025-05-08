5d1bcb9ce97894c89144631ec48158704f0e12eb
HaythamElbadwy <haythamelbadwy1997@gmail.com>
2025-05-08 12:20:16 +0300

    updated

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -8,7 +8,7 @@ export default function Footer() {
   useEffect(() => { }, [])
 
   return (
-    <footer className='w-full h-10 fixed bottom-0 z-50'>
+    <footer className={`${styles.masa_footer} w-full h-10 fixed bottom-0 z-50`}>
       <div className='mt-2 text-start flex items-center justify-center'>
         <i className="fa-regular fa-copyright"></i>
         <h1 className='ml-1 mb-1'>Copyright Masa,All Rights Reserved</h1>
